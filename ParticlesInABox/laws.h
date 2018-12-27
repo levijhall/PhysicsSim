499a671e40426d36d58a32d4126f40559dc9304b
Levi Hall <levi.j.hall@outlook.com>
2018-12-26 20:33:36 -0700

    removed unnecessary global variables

--- ParticlesInABox/laws.h
+++ ParticlesInABox/laws.h
@@ -3,8 +3,7 @@
 #include "core.h"
 
 //values
-const double g = -0.0001f;
-const double timeScale = 0.05f;
+const double g = -0.0001f * 0.f;
 
 //returns the new v1 value
 double ellasticCollision(double m1, double v1, double m2, double v2);
